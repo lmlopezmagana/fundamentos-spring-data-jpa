e6d87c0cb3c3fd859bc3f9dc77a83ba5ebec6345
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-18 15:45:47 +0200

    2.2 Identificador

--- spring-data/src/main/java/com/example/springdata/Producto.java
+++ spring-data/src/main/java/com/example/springdata/Producto.java
@@ -1,6 +1,8 @@
 package com.example.springdata;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class Producto {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     private String nombre;
