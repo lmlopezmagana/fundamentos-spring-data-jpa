33f9126de2ab8ca5e885e0d1895ad6aad4b4d53f
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-20 23:57:33 +0200

    2.5 equals y hashcode

--- spring-data/src/main/java/com/example/springdata/Producto.java
+++ spring-data/src/main/java/com/example/springdata/Producto.java
@@ -1,11 +1,17 @@
 package com.example.springdata;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -23,4 +29,20 @@ public class Producto {
 
     @Column(name = "precio")
     private double precioVenta;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Producto producto = (Producto) o;
+        return getId() != null && Objects.equals(getId(), producto.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
