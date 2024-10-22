67cf8dc6feeb69886b3709791fb0a23f10824158
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-22 23:09:12 +0200

    4.5 Fetching

--- spring-data/src/main/java/com/example/springdata/repos/CategoriaRepository.java
+++ spring-data/src/main/java/com/example/springdata/repos/CategoriaRepository.java
@@ -3,6 +3,17 @@ package com.example.springdata.repos;
 
 import com.example.springdata.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    @Query("""
+            select c from Categoria c
+                join fetch c.productos
+                where c.id = ?1
+            """)
+    Optional<Categoria> findCategoriaByIdWithProductos(Long id);
+
 }
