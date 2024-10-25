9b4f5d9f248680ab5126381028ad1d53055b7650
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-25 10:54:21 +0200

    9.1 Proyecto base

---
+++ todoapp-rest/src/main/java/com/openwebinars/data/todoapp/rest/TodoappRestApplication.java
@@ -0,0 +1,13 @@
+package com.openwebinars.data.todoapp.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappRestApplication.class, args);
+	}
+
+}
---
+++ todoapp-rest/src/test/java/com/openwebinars/data/todoapp/rest/TodoappRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openwebinars.data.todoapp.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
